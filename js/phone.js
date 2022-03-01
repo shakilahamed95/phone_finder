14a8e0f5ef0084d193d1c17e570ab65f2702b67a
Your Name <you@example.com>
2022-03-01 13:26:41 +0600

    search part done

--- js/phone.js
+++ js/phone.js
@@ -5,5 +5,30 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => displayPhone(data.data))
+
+}
+const displayPhone = (phones) => {
+    const searchResult = document.getElementById("search-result")
+    searchResult.textContent = '';
+    phones.forEach(phone => {
+        console.log(phone)
+        const div = document.createElement('div');
+        div.classList.add("col-lg-4")
+        div.classList.add("col-md-4")
+        div.classList.add("col-sm-6")
+        div.classList.add("col-12")
+        div.classList.add("mb-5")
+        div.innerHTML = `
+            <div class="card" style="width: 18rem;">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.brand}</h5>
+                <p class="card-text">${phone.phone_name}</p>
+                <button class="btn btn-primary">See Details</button>
+            </div>
+        </div>
+                `
+        searchResult.appendChild(div);
+    })
 }
